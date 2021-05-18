fba3551d89689f8f25ef47c1c119b23dc40db5f3
jokerjoker10 <36204071+jokerjoker10@users.noreply.github.com>
2021-05-18 15:59:10 +0200

    Cleaning with Defaults

--- Plantbotter/settings.h
+++ Plantbotter/settings.h
@@ -11,13 +11,14 @@ You have to enter a valid server Address and API Key.
 For a valid wifi connection enter your wifi ssid and wifi password 
 */
 
-//cloud server
-#define SERVER "http://192.168.178.44:3001/"
-#define API_KEY "pBUT8baBgi91KBK4ZEFNYOMJlwmcrYZD6RqAszN82l7CxrdqLg3rGS03M4s7hk0Fg4DdXgeHJ5mFlvriangZFafQvXUq3hfMfFo3ZCe5iPcEXgSIS0bCd96DyBFw"
+//Cloud Server
+//Server address with this syntax: <Address>:<Port>
+#define SERVER "SERVER:PORT"
+#define API_KEY "API_KEY"
 
 //wifi
-#define WIFI_SSID "Samhain"
-#define WIFI_PASSWORD "Funkenflug_1842"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
 
 
 // Soon Depretiated
