9366dc640f7144f3a69f6d56319ee53d1f12459e
jokerjoker10 <josefkerres@gmx.de>
2021-09-18 13:56:37 +0200

    Add Mail Verification

--- Cloud/ServerV2/Controller/mail.controller.js
+++ Cloud/ServerV2/Controller/mail.controller.js
@@ -8,7 +8,80 @@ var mailController = {
 }
 
 function verifyEmail(req, res){
+    if(!req.body.key || !req.body.email){
+        res.status(400)
+        .json({
+            message: "Key and email are required"
+        }); 
+    }
 
+    user_controller.getUserByMail(req.body.email)
+    .then((user) => {
+        if(user.dataValues.email_confirmed){
+            res.status(400)
+            .json({
+                message: "Email already confirmed"
+            });
+            return;
+        }
+
+        mail_model.findOne({where: {key: req.body.key}})
+        .then((mail_code) => {
+            //check if code belongs to mail and if mail_code is found and if mail_type is mail_verification
+            if(mail_code == null || mail_code.dataValues.userId != user.dataValues.id || mail_code.dataValues.mail_type != 'mail_verification'){
+                res.status(400)
+                .json({
+                    message: "Code unknown"
+                });
+                return;
+            }
+
+            if(!mail_code.dataValues.active){
+                res.status(400)
+                .json({
+                    message: "Code inactive"
+                });
+                return;
+            }
+
+            //activating user
+            user_controller.activateMail(user.id)
+            .then((updated_user) => {
+                //deactivating code
+                mail_model.update({active: false},{where: {id: mail_code.dataValues.id}})
+                .then((updated_mail_code) => {
+                    res.status(200)
+                    .json({
+                        message: "Account Activated"
+                    });
+                    return;
+                })
+                .catch((error) => {
+                    res.status(500)
+                    .json({
+                        message: "Deactivating code failed"
+                    });
+                    return;
+                });
+            })
+            .catch((error) => {
+                res.status(500)
+                .json({
+                    message: "Activating mail failed",
+                    error: error
+                });
+                return;
+            });            
+        })
+        .catch((error) => {
+            res.status(500)
+            .json({
+                message: "Finding Code Failed",
+                error: error
+            });
+            return;
+        });
+    });
 }
 
 function sendMailVerification(req, res){
