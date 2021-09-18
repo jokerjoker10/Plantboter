5cf5a2cb26ea75bf5440ecbf6e131025a1e9d826
jokerjoker10 <josefkerres@gmx.de>
2021-09-18 11:46:43 +0200

    Add GetUser Route

--- Cloud/ServerV2/Routes/user.route.js
+++ Cloud/ServerV2/Routes/user.route.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userController = require('../Controller/users.controller');
+const auth = require('../Utils/Middleware/auth');
 
 router.post('/', userController.addUser);
 router.post('/login', userController.loginUser)
+router.get('/getUser',auth, userController.getUser)
 
 module.exports = router;
