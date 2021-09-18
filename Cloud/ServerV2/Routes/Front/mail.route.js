b87492b0830f039e58c4983db03700cb50abdb1c
jokerjoker10 <josefkerres@gmx.de>
2021-09-18 13:57:16 +0200

    Add Verify Mail

--- Cloud/ServerV2/Routes/Front/mail.route.js
+++ Cloud/ServerV2/Routes/Front/mail.route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const mailController = require('../../Controller/mail.controller');
 
 router.get('/requestmailverification', mailController.sendMailVerification);
-router.post('/verifymail', mailController.verifyEmail)
+router.post('/verifymail', mailController.verifyEmail);
 
 module.exports = router;
