91dd12aa0a1dd723009363d4d54ccc4a4687fa4a
jokerjoker10 <josefkerres@gmx.de>
2021-09-30 13:39:57 +0200

    Remove Cookies from Login

--- Cloud/ServerV2/Controller/auth.controller.js
+++ Cloud/ServerV2/Controller/auth.controller.js
@@ -84,8 +84,7 @@ function login(req, res){
                     access_token: access_token,
                     refresh_token: refresh_token
                 })
-                .setHeader("x-access-token", access_token)
-                .cookie("refresh_token", refresh_token, {httpOnly: true});
+                .setHeader("x-access-token", access_token);
                 return;
             }
             else {
