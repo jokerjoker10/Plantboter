40dfcd8b914fb53b934037539e71dc5233ff6d95
jokerjoker10 <josefkerres@gmx.de>
2021-10-21 14:51:43 +0200

    Rework Init and Include DB Sync and DB Auth

--- Cloud/ServerV2/Init/init.js
+++ Cloud/ServerV2/Init/init.js
@@ -1,33 +1,82 @@
-const settings = require('../Utils/settings').getSettings();
+const config = require('../Utils/settings');
 const keyToken = require('./tokenkey');
 const mailer = require('../Utils/mail');
+const icons = require('../Utils/consoleEmoji');
+const db = require('../Database/database');
 
-async function initServer(){
-    // testing if token key ar available
-    if(!process.env.ACCESS_TOKEN_KEY || !process.env.REFRESH_TOKEN_KEY){
-        console.log("\n##### Access or Refresh Token Not Found #####");
-        console.log("For Security Reasons both codes will be renewed")
-        console.log("Generating new keys...\n");
+const initServer = () => {
+    return new Promise((resolve, reject) => {
 
-        process.env.ACCESS_TOKEN_KEY = keyToken.createTokenKey();
-        process.env.REFRESH_TOKEN_KEY = keyToken.createTokenKey();
+        var message = '';
 
-        console.log('The Access Token Keys need to be the same on all instances');
-        console.log('############### Access Token Key:\n' + process.env.ACCESS_TOKEN_KEY + '\n');
-        console.log('############### Refresh Token Key:\n' + process.env.REFRESH_TOKEN_KEY + '\n');
-    }
+        console.log('\n##### Initializing Server! #####')
+        {
+            // testing if token key ar available
+            process.stdout.write('Token Keys: ');
+            var keys = config.getTokenKeys();
+            if (!keys.access_token_key || !keys.refresh_token_key) {
 
-    // testing if mail connection is avialable
-    console.log('##### Testing Mail Service #####\n')
-    try{
-        await mailer.sendTestMail();
-    }
-    catch(error){
+                var access_token_key = keyToken.createTokenKey();
+                var refresh_token_key = keyToken.createTokenKey();
 
-        throw 'Error: Mail connection false: ' + error;
-    };
+                config.saveTokenKey(access_token_key, refresh_token_key);
 
-    console.log('\n##### Mail Connection Successfull #####\n')
+
+                message += '\n##### Access or Refresh Token Not Found #####\n';
+                message += 'For Security Reasons both codes will be renewed\n';
+                message += 'The Access Token Keys need to be the same on all instances\n';
+                message += '############### Access Token Key:\n' + access_token_key + '\n';
+                message += '############### Refresh Token Key:\n' + refresh_token_key + '\n\n';
+                config.getSettings();
+                console.log(icons.warning);
+            }
+            else{
+                console.log(icons.success)
+            }
+        }
+        {
+            // testing if mail connection is avialable
+            process.stdout.write('Mail Service: ');
+
+            mailer.sendTestMail()
+                .then(() => {
+                    console.log(icons.success);
+
+                    //testing mail
+                    process.stdout.write('Database Auth: ');
+                    db.authenticate()
+                        .then(() => {
+                            console.log(icons.success);
+
+                            process.stdout.write('Database Sync: ');
+                            // sync database
+                            db.sync()
+                            .then(() => {
+                                console.log(icons.success);
+                                console.log('##### Initializing done! #####');
+                                console.log(message);
+                                resolve();
+                            })
+                            .catch(err => {
+                                console.log(icons.error)
+                                reject('Database Sync failed: ' + err);
+                                return;
+                            });
+                        })
+                        .catch(err => {
+                            console.log(icons.error)
+                            reject('Database Connection Error: ' + err);
+                            return;
+                        });
+                })
+                .catch((error) => {
+                    console.log(icons.error)
+                    reject('Error: Mail connection false: ' + error);
+                    return;
+                });
+        }
+    });
+
 }
 
-module.exports = {initServer: initServer}
+module.exports = { initServer: initServer }
