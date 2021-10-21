e65f5c3a2011f939fd2bb29583b38203c435325f
jokerjoker10 <josefkerres@gmx.de>
2021-10-21 14:53:53 +0200

    Move DB Auth and Add Waiting for init

--- Cloud/ServerV2/index.js
+++ Cloud/ServerV2/index.js
@@ -1,35 +1,33 @@
 const PORT = process.env.PORT || 3001;
 
-const server = require('./Routes/app');
 const pkg = require('./package.json');
 const gitVersion = require('git-tag-version');
 const init = require('./Init/init');
 
-// Connect to Database
-const db = require('./Database/database');
-db.authenticate()
+init.initServer()
     .then(() => {
-        console.log('\nDatabase Connected\n');
+        // load modules after init
+        const server = require('./Routes/app');
+        const settings = require('./Utils/settings').getSettings();
+
+        // Start Server
+        server.listen(PORT, () => {
+            console.log(`\n\n###############################################`);
+            console.log(`${pkg.name} Online. Listening on Port ${PORT}`);
+            console.log(`Version: ${gitVersion()}`);
+            console.log(`Listening on: ${settings.domains.api}`);
+            console.log(`###############################################\n\n`);
+        })
+
+        // Close Connection to database on Shutrown
+        process.on('SIGTERM', () => {
+            console.log(`${pkg.name} Shut down recieved`);
+            process.exit(0);
+        })
+
+        module.exports = server;
     })
-    .catch(err => {
-        throw 'Database Connection Error: ' + err;
+    .catch((error) => {
+        console.log(error);
+        return;
     });
-
-console.log('\n### Initialising Server ###\n');
-init.initServer();
-
-// Start Server
-server.listen(PORT, () => {
-    console.log(`###############################################`);
-    console.log(`${pkg.name} Online. Listening on Port ${PORT}`);
-    console.log(`${pkg.name} Version: ${gitVersion()}`)
-    console.log(`###############################################\n\n`);
-})
-
-// Close Connection to database on Shutrown
-process.on('SIGTERM', () => {
-    console.log(`${pkg.name} Shut down recieved`);
-    process.exit(0);
-})
-
-module.exports = server;
