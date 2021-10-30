9ba1c529453902545b91d7b1f67c1ad59e88b185
jokerjoker10 <36204071+jokerjoker10@users.noreply.github.com>
2021-10-30 10:15:57 +0200

    Fix broken password compare

--- Cloud/PlantboterServer/Utils/crypt.js
+++ Cloud/PlantboterServer/Utils/crypt.js
@@ -2,26 +2,29 @@ const bcrypt = require('bcrypt');
 
 // encrypts passwodd to be saved in database
 function cryptPassword(password, callback) {
-    bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.genSalt(10, function (err, salt) {
         if (err)
             return callback(err);
 
-        bcrypt.hash(password, salt, function(err, hash) {
+        bcrypt.hash(password, salt, function (err, hash) {
             return callback(err, hash);
         });
     });
 }
 
 // compare a plain password to a hashed to find out if it 
-let comparePassword = new Promise(function(plainpass, hashword) {
-    bcrypt.compare(plainpass, hashword, (err, result) => {
-        if(err){
-            throw err;
-        }
-        
-        return result;
-    });
-});
+const comparePassword = (plainpass, hashword) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(plainpass, hashword, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(result);
+        });
+    })
+
+};
 
 module.exports = {
     cryptPassword,
