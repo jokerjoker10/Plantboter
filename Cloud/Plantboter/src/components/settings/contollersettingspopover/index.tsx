fc884046cd526ede4b8bcb9eda740a8b3b83aa1d
jokerjoker10 <36204071+jokerjoker10@users.noreply.github.com>
2021-12-01 22:07:31 +0100

    Remove Unused Links and add link to Api Modal

--- Cloud/Plantboter/src/components/settings/contollersettingspopover/index.tsx
+++ Cloud/Plantboter/src/components/settings/contollersettingspopover/index.tsx
@@ -1,17 +1,17 @@
-import { IonButton, IonCard, IonCardContent, IonIcon, IonItem, IonList, IonListHeader } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonIcon, IonItem, IonList, IonListHeader, useIonModal,  } from '@ionic/react';
 import { codeOutline, enterOutline, exitOutline, keyOutline, trashBinOutline } from 'ionicons/icons';
 import React from 'react';
+import ApiModal from '../apimodal';
 
 const ControllerSettingsPopover: React.FC<{
+    controller_id: Number;
     onHide: () => void;
-}> = ({ onHide }) => {    
+}> = ({ onHide, controller_id }) => {
+    const [openApiPopover, dismiss] = useIonModal(ApiModal, { onDismiss: () => dismiss(), controller_id: controller_id });
     return (
         <IonList>
             <IonListHeader>Contoller Actions</IonListHeader>
-            <IonItem button><IonIcon icon={keyOutline}></IonIcon>Api Key : TODO</IonItem>
-            <IonItem button><IonIcon icon={codeOutline}></IonIcon>Generate Config : TODO</IonItem>
-            <IonItem button><IonIcon icon={exitOutline}></IonIcon>Export Controller : TODO</IonItem>
-            <IonItem button><IonIcon icon={enterOutline}></IonIcon>Import Controller : TODO</IonItem>
+            <IonItem button onClick={e => openApiPopover()}><IonIcon icon={keyOutline}></IonIcon>Api Key</IonItem>
             <IonItem button color="danger"><IonIcon icon={trashBinOutline}></IonIcon>Delete Controller : TODO</IonItem>
         </IonList>
     );
