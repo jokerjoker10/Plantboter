b8efd6e645ab9de9ed7cd16e388444068072d091
jokerjoker10 <36204071+jokerjoker10@users.noreply.github.com>
2021-12-01 22:06:25 +0100

    Removed unused Import

--- Cloud/PlantboterServer/Routes/Front/controller.route.js
+++ Cloud/PlantboterServer/Routes/Front/controller.route.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const authController = require('../../Controller/auth.controller');
 const controllerController = require('../../Controller/controller.controller');
 
 router.get('/', controllerController.getControllerList);
